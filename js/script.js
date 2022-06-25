43f071c34caef54f88279ef4715b19d0e6d963da
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-06-25 09:30:48 +0000

    Added global array and initPrimes function at script.js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Set Up Global Array
+const PRIMES = Array(50000).fill(false);
+(function initPrimes(num) {
+    const upper = Math.floor((num-1)/2);
+    const isPrime = Array(upper).fill(true);
+    const sqrtUpper = Math.floor((Math.sqrt(num)-1)/2);
+    for (let i=0;i<=sqrtUpper;i++) {
+        if (isPrime[i]) {
+            const prime = 2 * i + 3;
+            PRIMES[prime] = true;
+            const primeSquaredIndex = 2 * i ** 2 + 6 * i + 3;
+            for (let j=primeSquaredIndex;j<upper;j+=prime) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    for (let i=sqrtUpper+1;i<upper;i++) {
+        if (isPrime[i]) PRIMES[2*i+3] = true;
+    }
+})(50000);
